eeaf1bf30813837d84b498370be98b289bfd44a5
nalger2 <nalger2@zagmail.gonzaga.edu>
2022-12-11 16:28:18 -0800

    added conf matrix

--- projects/naive bayes/utilFunctions.py
+++ projects/naive bayes/utilFunctions.py
@@ -28,3 +28,53 @@ def makeBagOfWords(review):
     words = [word for word in words if word not in stop_words] #stop words removed
     bag = bag + words
     return bag
+
+def confusion_matrix(y_true, y_pred, labels):
+    """Compute confusion matrix to evaluate the accuracy of a classification.
+    Args:
+        y_true(list of obj): The ground_truth target y values
+            The shape of y is n_samples
+        y_pred(list of obj): The predicted target y values (parallel to y_true)
+            The shape of y is n_samples
+        labels(list of str): The list of all possible target y labels used to index the matrix
+    Returns:
+        matrix(list of list of int): Confusion matrix whose i-th row and j-th column entry
+            indicates the number of samples with true label being i-th class
+            and predicted label being j-th class
+    Notes:
+        Loosely based on sklearn's confusion_matrix():
+            https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+    """
+    matrix = []
+    for label in labels:
+        row = [0 for label in labels] #[0, 0, 0]
+        matrix.append(row) #[[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    for i in range(len(labels)): #check true
+        for j in range(len(labels)): #check predicted
+            for n in range(len(y_true)): #iterate through number of samples
+                if y_true[n] == labels[i] and y_pred[n] == labels[j]:
+                    matrix[i][j] += 1
+    return matrix
+
+def print_binary_confusion_matrix_labels(matrix):
+    """Returns positional labels for each location in a binary matrix, assuming pos label
+    is at positions 0 and 0
+
+    Args:
+        matrix(list list int): confusion matrix for a classifier
+
+    Returns:
+        tp (int): true positives
+        fp (int): false positives
+        tn (int): true negatives
+        fn (int): false negatives
+    """
+    tp = matrix[0][0]
+    print("true positives TP:", tp)
+    fp = matrix[1][0]
+    print("false positives FP:", fp)
+    tn = matrix[1][1]
+    print("true negatives TN:", tn)
+    fn = matrix[0][1]
+    print("false negatives:", fn)
+    return tp, fp, tn, fn
